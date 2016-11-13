aa45714fd9f3cb1f184bf906dba27ecee4068194
johnnyzh82 <johnnyzh82@gmail.com>
2016-11-13 15:24:25 -0500

    Initial commit asp.ent

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialLife.Startup))]
+namespace SocialLife
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
